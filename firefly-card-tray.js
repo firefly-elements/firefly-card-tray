d10981652a96839a4d0d54c9dc84c65a4af22bd0
Marko Ignjatovic <markoafx@gmail.com>
2019-11-19 18:24:10 +0100

    Remove firefly-tray-mixin

--- firefly-card-tray.js
+++ firefly-card-tray.js
@@ -1,14 +1,13 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import '@polymer/iron-flex-layout/iron-flex-layout.js';
-import '@polymer/paper-fab/paper-fab.js';
-import '@polymer/iron-list/iron-list.js';
-import '@aspen-elements/aspen-icons';
-
-import { AspenSecurableMixin } from '@aspen-elements/aspen-securable-mixin';
-import '@firefly-elements/firefly-delete-dialog';
-import { FireflyTrayMixin } from '@firefly-elements/firefly-tray-mixin';
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
-
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import "@polymer/iron-flex-layout/iron-flex-layout.js";
+import "@polymer/paper-fab/paper-fab.js";
+import "@polymer/iron-list/iron-list.js";
+import "@aspen-elements/aspen-icons";
+import { matchesSelector } from "@polymer/polymer/lib/legacy/polymer.dom.js";
+import { FlattenedNodesObserver } from "@polymer/polymer/lib/utils/flattened-nodes-observer.js";
+import { AspenSecurableMixin } from "@aspen-elements/aspen-securable-mixin";
+import "@firefly-elements/firefly-delete-dialog";
+import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
 
 /**
  * `firefly-tray` This component is used to render a collection of cards. Each card must have the "card" CSS class,
@@ -19,82 +18,95 @@ import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
  * @polymer
  * @extends {Polymer.Element}
  */
-class FireflyCardTray extends AspenSecurableMixin(FireflyTrayMixin(PolymerElement)) {
+class FireflyCardTray extends AspenSecurableMixin(PolymerElement) {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+          --paper-fab-bottom: 10px;
+          --paper-fab-right: 10px;
+        }
 
-	static get template() {
-		return html`
-        <style>
-			:host {
-				display: block; 
-				--paper-fab-bottom: 10px;
-				--paper-fab-right: 10px;
-			}
+        .tray {
+          @apply --layout-horizontal;
+          @apply --layout-wrap;
+        }
 
-			.tray{
-				@apply --layout-horizontal;
-				@apply --layout-wrap;
-			}
+        paper-fab {
+          position: fixed;
+          bottom: var(--paper-fab-bottom);
+          right: var(--paper-fab-right);
+        }
+      </style>
 
-			paper-fab{
-				position: fixed;
-				bottom: var(--paper-fab-bottom);
-				right: var(--paper-fab-right);
-			}
+      <div class="tray">
+        <slot select=".card"></slot>
+        <template is="dom-if" if="[[hasRole]]">
+          <paper-fab
+            icon="aspen:add"
+            on-tap="_openAddDialog"
+            mini="[[fabIsMini]]"
+          ></paper-fab>
+        </template>
+      </div>
 
-		</style>
+      <slot select=".detail-dialog"></slot>
 
-		<div class="tray">
-			<slot select=".card"></slot>
-			<template is="dom-if" if="[[hasRole]]">
-				<paper-fab icon="aspen:add" on-tap="_openAddDialog" mini="[[fabIsMini]]"></paper-fab>
-			</template>
-		</div>
-	
-		<slot select=".detail-dialog"></slot>
-		
-		<firefly-delete-dialog></firefly-delete-dialog>
-        `;
-	}
+      <firefly-delete-dialog></firefly-delete-dialog>
+    `;
+  }
 
+  /**
+   * String providing the tag name to register the element under.
+   */
+  static get is() {
+    return "firefly-card-tray";
+  }
 
-    /**
-     * String providing the tag name to register the element under.
-     */
-	static get is() {
-		return 'firefly-card-tray';
-	}
+  static get properties() {
+    return {
+      /** This flag determines which of the icons selected/deselected are shown"*/
+      data: {
+        type: Boolean,
+        value: false
+      }
+    };
+  }
 
-	static get properties() {
-		return {
-			/** This flag determines which of the icons selected/deselected are shown"*/
-			data: {
-				type: Boolean,
-				value: false
-			}
-		}
-	}
+  /**
+   * Instance of the element is created/upgraded. Use: initializing state,
+   * set up event listeners, create shadow dom.
+   * @constructor
+   */
+  constructor() {
+    super();
+  }
 
-	/**
-	 * Instance of the element is created/upgraded. Use: initializing state,
-	 * set up event listeners, create shadow dom.
-	 * @constructor
-	 */
-	constructor() {
-		super();
-	}
+  /**
+   * Method for opening dialog by clicking on a fab button
+   *
+   * @memberof FireflyCardTray
+   */
+  _openAddDialog() {
+    let nodes = FlattenedNodesObserver.getFlattenedNodes(this);
+    let assignedNodes = nodes.filter(
+      n =>
+        n.nodeType === Node.ELEMENT_NODE && matchesSelector(n, ".detail-dialog")
+    );
+    let dialog = assignedNodes[0];
+    console.log(dialog);
+    dialog.newOpen();
+  }
 
+  /**
+   * Use for one-time configuration of your component after local DOM is initialized.
+   */
+  ready() {
+    super.ready();
 
-    /**
-     * Use for one-time configuration of your component after local DOM is initialized. 
-     */
-	ready() {
-		super.ready();
-
-		afterNextRender(this, function () {
-
-		});
-	}
-
+    afterNextRender(this, function() {});
+  }
 }
 
 window.customElements.define(FireflyCardTray.is, FireflyCardTray);
